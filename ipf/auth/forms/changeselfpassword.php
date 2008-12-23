130c560e0fe91bc62a9b3ec8415ed0b4f7378020
avl <alex.litovchenko@gmail.com>
2008-12-23 12:48:47 +0200

    fix IPF_Auth_Forms_ChangeSelfPassword

--- ipf/auth/forms/changeselfpassword.php
+++ ipf/auth/forms/changeselfpassword.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
 class IPF_Auth_Forms_ChangeSelfPassword extends IPF_Form{
-    
+
     function initFields($extra=array())
     {
-        $this->fields['oldpassword'] = new IPF_Form_Field_Varchar(array(label=>'Current Password', 'required'=>true, 'widget'=>'IPF_Form_Widget_PasswordInput'));
-        $this->fields['password1'] = new IPF_Form_Field_Varchar(array(label=>'New Password', 'required'=>true,'widget'=>'IPF_Form_Widget_PasswordInput'));
-        $this->fields['password2'] = new IPF_Form_Field_Varchar(array(label=>'New Password (repeat)','required'=>true,'widget'=>'IPF_Form_Widget_PasswordInput','help_text'=>'Enter the same password as above, for verification.'));
+        $this->fields['oldpassword'] = new IPF_Form_Field_Varchar(array('label'=>'Current Password', 'required'=>true, 'widget'=>'IPF_Form_Widget_PasswordInput'));
+        $this->fields['password1'] = new IPF_Form_Field_Varchar(array('label'=>'New Password', 'required'=>true,'widget'=>'IPF_Form_Widget_PasswordInput'));
+        $this->fields['password2'] = new IPF_Form_Field_Varchar(array('label'=>'New Password (repeat)','required'=>true,'widget'=>'IPF_Form_Widget_PasswordInput','help_text'=>'Enter the same password as above, for verification.'));
     }
-    
+
     function isValid($request){
         $ok = parent::isValid();
         if ($ok===true){
