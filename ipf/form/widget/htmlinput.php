0b3ef8030a1fe85ab7c46c7e28ec4f6378130479
avl <alex.litovchenko@gmail.com>
2009-01-29 13:25:43 +0200

    tablecontrols in  tiny_mce

--- ipf/form/widget/htmlinput.php
+++ ipf/form/widget/htmlinput.php
@@ -63,7 +63,7 @@ class IPF_Form_Widget_HTMLInput extends IPF_Form_Widget
 					}
             	tinyMCE.init({
                     theme_advanced_buttons1 : "bold, italic, separator, undo, redo, separator, bullist, numlist, outdent, indent, separator, justifyleft, justifycenter, justifyright, separator, link, unlink, forecolor, backcolor, sub, sup, separator, preview",
-                    theme_advanced_buttons2 : "code, fullscreen, image, link, charmap, separator, pastetext, pasteword, selectall, removeformat, separator, formatselect, fontselect, fontsizeselect",
+                    theme_advanced_buttons2 : "code, fullscreen, image, link, charmap, separator, pastetext, pasteword, selectall, removeformat, separator, formatselect, fontselect, fontsizeselect, separator, tablecontrols",
                     theme_advanced_buttons3 : "",
                     theme_advanced_toolbar_location : "top",
                     theme_advanced_toolbar_align: "left",
