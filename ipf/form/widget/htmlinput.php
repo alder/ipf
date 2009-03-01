68dcf3e1c1f17f5bdafba06c8e0d092335fa809f
avl <alex.litovchenko@gmail.com>
2009-03-01 13:38:40 +0200

    justify full

--- ipf/form/widget/htmlinput.php
+++ ipf/form/widget/htmlinput.php
@@ -62,8 +62,8 @@ class IPF_Form_Widget_HTMLInput extends IPF_Form_Widget
 					  window.open("/media/tiny_mce/plugins/kfm/index.php?mode=selector&lang=en&type="+type,"kfm","modal,width=800,height=600");
 					}
             	tinyMCE.init({
-                    theme_advanced_buttons1 : "bold, italic, underline, separator, undo, redo, separator, bullist, numlist, outdent, indent, separator, justifyleft, justifycenter, justifyright, separator, link, unlink, forecolor, backcolor, sub, sup, separator, preview",
-                    theme_advanced_buttons2 : "code, fullscreen, image, charmap, separator, pastetext, pasteword, selectall, removeformat, separator, formatselect, fontselect, fontsizeselect, separator",
+                    theme_advanced_buttons1 : "bold, italic, underline, separator, undo, redo, separator, bullist, numlist, outdent, indent, separator, justifyleft, justifycenter, justifyright, justifyfull, separator, link, unlink, forecolor, backcolor, sub, sup, separator, preview",
+                    theme_advanced_buttons2 : "code, fullscreen, image, charmap, separator, pastetext, pasteword, selectall, removeformat, separator, formatselect, fontselect, fontsizeselect",
                     theme_advanced_buttons3 : "tablecontrols",
                     theme_advanced_toolbar_location : "top",
                     theme_advanced_toolbar_align: "left",
