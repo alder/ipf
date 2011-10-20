e7cce741fdc47193d1e3e867a1c83c8f561b4b63
Andrey Kutejko <andy128k@gmail.com>
2011-10-20 13:05:11 +0200

    fix call-time pass-by-reference

--- ipf/orm/pager/layoutarrows.php
+++ ipf/orm/pager/layoutarrows.php
@@ -9,41 +9,51 @@ class IPF_ORM_Pager_LayoutArrows extends IPF_ORM_Pager_Layout
 
         $range = $this->getPagerRange()->rangeAroundPage();
 
-		if ($pager->getFirstPage()!=$pager->getLastPage()){
+        if ($pager->getFirstPage() != $pager->getLastPage())
+        {
+            $this->removeMaskReplacement('page');
 
-	        $this->removeMaskReplacement('page');
+            if ($range[0] > 1)
+            {
+                $options['page_number'] = 1;
+                $str .= $this->processPage($options);
 
-	        if ($range[0]>1){
+                if ($range[0] > 2)
+                {
+                    $options['page_number'] = 2;
+                    $str .= $this->processPage($options);
+                }
 
-		        $options['page_number'] = 1;
-		        $str .= $this->processPage($options);
+                if ($range[0]>3)
+                    $str .= ' ... ';
+            }
 
-				if ($range[0]>2){
-			        $options['page_number'] = 2;
-			        $str .= $this->processPage($options);
-				}
-				if ($range[0]>3)
-		        	$str .= ' ... ';
-	        }
+            // Pages listing
+            $str .= parent::display($options, true);
 
-	        // Pages listing
-	        $str .= parent::display(&$options, true);
-	        $last_range = $options['page_number'];
-	        if (($last_range)<$pager->getLastPage()){
+            $range = $this->getPagerRange()->rangeAroundPage();
+            $last_range = $range[count($range)-1];
 
-		        if (($last_range+2)<$pager->getLastPage()){
-		        	$str .= ' ... ';
-		        }
-		        if (($last_range+1)<$pager->getLastPage()){
-		        	$options['page_number'] = $pager->getLastPage()-1;
-			        $str .= $this->processPage($options);
-		        }
-		        if (($last_range)<$pager->getLastPage()){
-			        $options['page_number'] = $pager->getLastPage();
-			        $str .= $this->processPage($options);
-		        }
-	        }
-		}
+            if ($last_range < $pager->getLastPage())
+            {
+                if ($last_range < $pager->getLastPage() - 2)
+                {
+                    $str .= ' ... ';
+                }
+
+                if ($last_range < $pager->getLastPage() - 1)
+                {
+                    $options['page_number'] = $pager->getLastPage() - 1;
+                    $str .= $this->processPage($options);
+                }
+
+                if ($last_range < $pager->getLastPage())
+                {
+                    $options['page_number'] = $pager->getLastPage();
+                    $str .= $this->processPage($options);
+                }
+            }
+        }
 
         if ($return)
             return $str;
@@ -51,3 +61,4 @@ class IPF_ORM_Pager_LayoutArrows extends IPF_ORM_Pager_Layout
         echo $str;
     }
 }
+
