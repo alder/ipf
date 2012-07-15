bf6c9ddb922cdbb9e56ec80fde6b9e81c76c1273
Andrey Kutejko <andy128k@gmail.com>
2012-07-15 22:07:17 +0300

    suppress notice when rendering 404 page

--- ipf/http/response/notfound.php
+++ ipf/http/response/notfound.php
@@ -4,16 +4,21 @@ class IPF_HTTP_Response_NotFound extends IPF_HTTP_Response
 {
     function __construct($content='404 Not Found', $mimetype=null)
     {
-        try{
+        try
+        {
             $context = array(
-                'title'=>'404 Not Found',
-                'query_string'=>$_SERVER['QUERY_STRING'],
-                'MEDIA_URL'=>IPF::get('media_url'),
-                'ADMIN_MEDIA_URL'=>IPF::get('admin_media_url'),
+                'title' => '404 Not Found',
+                'query_string' => @$_SERVER['QUERY_STRING'],
+                'MEDIA_URL' => IPF::get('media_url'),
+                'ADMIN_MEDIA_URL' => IPF::get('admin_media_url'),
             );
             $content = IPF_Shortcuts::RenderToString('404.html', $context);
-        }catch(IPF_Exception $e){}
+        }
+        catch (IPF_Exception $e)
+        {
+        }
         parent::__construct($content, $mimetype);
         $this->status_code = 404;
     }
 }
+
