679b17b01e247368a17406ddb9d0c23d523ef693
Andrey Kutejko <andy128k@gmail.com>
2013-07-25 20:28:42 +0300

    enlarge numeric widgets

--- ipf/form/db/double.php
+++ ipf/form/db/double.php
@@ -1,8 +1,11 @@
 <?php
 
-class IPF_Form_DB_Double extends IPF_Form_DB{
-    function formField($def, $form_field='IPF_Form_Field_Float'){
-        $def['widget_attrs'] = array('style'=>'width:40px;');
+class IPF_Form_DB_Double extends IPF_Form_DB
+{
+    function formField($def, $form_field='IPF_Form_Field_Float')
+    {
+        $def['widget_attrs'] = array('style' => 'width:140px;');
         return parent::formField($def, $form_field);
     }
 }
+
