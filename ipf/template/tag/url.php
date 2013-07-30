87043cab7b72503d0b96a8fd82e8f362e0f2bb74
Andrey Kutejko <andy128k@gmail.com>
2013-07-30 20:26:39 +0300

    better url template tag. no need to wrap parameters into array

--- ipf/template/tag/url.php
+++ ipf/template/tag/url.php
@@ -2,9 +2,22 @@
 
 class IPF_Template_Tag_Url extends IPF_Template_Tag
 {
-    function start($view, $params=array(), $get_params=array())
+    function start()
     {
-        echo IPF_HTTP_URL::urlForView($view, $params, $get_params);
+        $args = func_get_args();
+        $count = count($args);
+        if ($count === 0)
+            throw new IPF_Exception('No view specified');
+
+        $view = array_shift($args);
+
+        if ($count === 2 && is_array($args[0])) {
+            echo IPF_HTTP_URL::urlForView($view, $args[0]);
+        } elseif ($count === 3 && is_array($args[0]) && is_array($args[1])) {
+            echo IPF_HTTP_URL::urlForView($view, $args[0], $args[1]);
+        } else {
+            echo IPF_HTTP_URL::urlForView($view, $args);
+        }
     }
 }
 
