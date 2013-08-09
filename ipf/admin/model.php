167888c0142757f5dadabbdce4b2c218c0656540
Aleksey Derkach <aleksey.derkach@gmail.com>
2013-08-09 07:07:27 +0300

    Don't set default timezone and use only DateTime class methods

--- ipf/admin/model.php
+++ ipf/admin/model.php
@@ -447,9 +447,8 @@ class IPF_Admin_Model
                     case 'timestamp':
                         $date = DateTime::createFromFormat('Y-m-d', $str);
                         if ($date == false) {
-                            date_default_timezone_set('UTC');
-                            $date = new DateTime("@".strtotime ($str));
-                            $date->setTimeZone(new DateTimezone(IPF::get('time_zone')));
+                            $date = new DateTime($str, new DateTimezone('UTC'));
+                            $date->setTimeZone(new DateTimezone('Europe/Kiev'));
                             $str = gmdate('Y-m-d H:i:s', $date->format('U') + $date->getOffset());
                         }
                         break;
